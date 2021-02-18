71b7fc36fd149f4de6d4046d40eaae45115f9074
d00d1e <lee.julianay@gmail.com>
2021-02-18 00:38:14 -0800

    add redux to productView

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,15 @@
-import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore, compose, applyMiddleware, combineReducers } from "redux";
+import thunk from "redux-thunk";
+import { productDetailsReducer } from "./reducers/productReducers";
 import { productListReducer } from "./reducers/productReducers";
 
 const initialState = {};
 
 //introduce reducers to store
 const reducer = combineReducers({
-  productList: productListReducer
-})
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+});
 
 // connect app to store (see contents of state in redux devtools)
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -15,6 +17,7 @@ const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   reducer,
   initialState,
-  composeEnhancer(applyMiddleware(thunk)));
+  composeEnhancer(applyMiddleware(thunk))
+);
 
 export default store;

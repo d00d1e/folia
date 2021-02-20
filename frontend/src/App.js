4898c87be4845c1e085394663f7e8660edb1017d
d00d1e <lee.julianay@gmail.com>
2021-02-20 02:11:09 -0800

    create payment view component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import SigninView from "./views/SigninView";
 import { signout } from "./actions/userActions";
 import RegisterView from "./views/RegisterView";
 import ShippingView from "./views/ShippingView";
+import PaymentView from "./views/PaymentView";
 
 export default function App() {
   const cart = useSelector((state) => state.cart);
@@ -63,6 +64,7 @@ export default function App() {
           <Route exact path="/signin" component={SigninView} />
           <Route exact path="/register" component={RegisterView} />
           <Route exact path="/shipping" component={ShippingView} />
+          <Route exact path="/payment" component={PaymentView} />
           <Route exact path="/" component={HomeView} />
         </main>
         <footer className="row center">All rights reserved</footer>

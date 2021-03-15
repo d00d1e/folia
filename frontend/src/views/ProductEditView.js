8848ac9ee9ef91d5b5e2c03e059e9e45f554b684
d00d1e <lee.julianay@gmail.com>
2021-03-14 19:26:28 -0700

    implement image upload route

--- frontend/src/views/ProductEditView.js
+++ frontend/src/views/ProductEditView.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { detailsProduct, updateProduct } from "../actions/productActions";
 import LoadingBox from "../components/LoadingBox";
@@ -60,6 +61,31 @@ export default function ProductEditView(props) {
     );
   };
 
+  const [loadingUpload, setLoadingUpload] = useState(false);
+  const [errorUpload, setErrorUpload] = useState("");
+
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  const uploadFileHandler = async (e) => {
+    const file = e.target.files[0];
+    const bodyFormData = new FormData();
+    bodyFormData.append("image", file);
+    setLoadingUpload(true);
+    try {
+      const { data } = await Axios.post("/api/uploads", bodyFormData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      });
+      setImage(data);
+      setLoadingUpload(false);
+    } catch (error) {
+      setErrorUpload(error.message);
+      setLoadingUpload(false);
+    }
+  };
+
   return (
     <div>
       <form className="form" onSubmit={submitHandler}>
@@ -114,6 +140,19 @@ export default function ProductEditView(props) {
                 onChange={(e) => setImage(e.target.value)}
               />
             </div>
+            <div>
+              <label htmlFor="image">Image File</label>
+              <input
+                type="file"
+                id="imageFile"
+                label="Choose Image"
+                onChange={uploadFileHandler}
+              />
+            </div>
+            {loadingUpload && <LoadingBox />}
+            {errorUpload && (
+              <MessageBox variant="danger">{errorUpload}</MessageBox>
+            )}
             <div>
               <label htmlFor="lighting">Lighting</label>
               <input

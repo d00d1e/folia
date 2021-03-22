7abc29039581ef724cd6443931d862396292c2ed
d00d1e <lee.julianay@gmail.com>
2021-03-22 11:18:33 -0700

    publish to heroku

--- backend/server.js
+++ backend/server.js
@@ -30,10 +30,14 @@ app.get("/api/config/paypal", (req, res) => {
 
 const __dirname = path.resolve(); //returns current folder
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+app.use(express, static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
 
-app.get("/", (req, res) => {
-  res.send("Server is ready");
-});
+// app.get("/", (req, res) => {
+//   res.send("Server is ready");
+// });
 
 // error handler middleware
 app.use((err, req, res, next) => [
